ba2abb8ffd4ce492f0d1646941f5b3cd1dc749e4
Weikai Zhang <weikai@gmail.com>
2020-07-29 16:44:34 -0400

    Updated to take testing data file path

--- importxml.py
+++ importxml.py
@@ -1,35 +1,39 @@
-from os import listdir
+import os,sys
 from includes.census import *
 
 def list_files(directory, extension):
-    return (f for f in listdir(directory) if f.endswith('.' + extension))
+    
+    files=[]
+    for f in os.listdir(directory):
+        if f.endswith('.' + extension):
+            files.append(f)
+
+    files.sort()
+    return files
 
 
-
-def main():     
+def main(file=''):     
     census = Census(year=1940)
 
     datapath = 'data/XML'
-    # parse xml file 
-    file=None
-    #census.parseXML('data/XML/AK.xml')  
-    #census.parseXML('data/XML/AL.xml')
-    #census.parseXML('data/XML/NE.xml')
-    #file = 'data/XML/NE.xml'
-    file = 'data/XML/AK.xml'
+    
     if file:
-        census.parseXML(file)
+        print(f"Processing {os.path.join(datapath, file)}")
+        census.parseXML(file)        
     else:
-        files = list_files(datapath, "xml")
-        total=len(files)
+        files = list_files(datapath, "xml")        
+        
+        total=len(files)        
         c=1        
         for file in files:                                
             print(f"{c}/{total}: Processing {os.path.join(datapath, file)}")
             census.parseXML(os.path.join(datapath, file))
+            c+=1
     
-if __name__ == "__main__": 
-  
-    # calling main function 
-    main() 
+if __name__ == "__main__":     
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        main()
     
 
